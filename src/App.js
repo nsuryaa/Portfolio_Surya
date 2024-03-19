d5e1546f8551dcbfedf1b05ef4f68e18c034158a
nsuryaa <suryasedulous@gmail.com>
2024-03-19 16:58:01 +0530

    added project section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hero/>
       <About/>
+      <Projects/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       borderColor:{
-        'primary':'rgb(85 81 227)'
+        'primary':'rgb(85 81 227)',
+        'secondary':'#2b2d77',
       }
     },
     fontFamily:{
