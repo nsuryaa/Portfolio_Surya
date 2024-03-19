861c4e518148c2b457ae1a9f41f4038e7a7b14bb
nsuryaa <suryasedulous@gmail.com>
2024-03-19 12:46:21 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500">Bye World</h1>
+    </div>
+  );
+}
+
+export default App;
