e024703430cd7b58a8875c9d867aecf85a6821fe
nsuryaa <suryasedulous@gmail.com>
2024-03-19 17:22:56 +0530

    added contact section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
@@ -12,6 +13,7 @@ function App() {
       <About/>
       <Projects/>
       <Resume/>
+      <Contact/>
     </div>
   );
 }
